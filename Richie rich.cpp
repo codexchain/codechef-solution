d885525c52c2d68d288a04d9c10486d6ddbb05db
Uzumaki Hokage <63039157+codexchain@users.noreply.github.com>
2021-08-11 10:22:40 +0530

    Add files via upload

---
+++ Richie rich.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define N '\n'
+#define pb push_back
+#define s(n) scanf("%d",&n)
+#define sl(n) scanf("%ld",&n)
+#define MX INT_MAX
+#define MN INT_MIN
+#define d(x) cout << #x <<" "<<x <<'\n'
+
+using namespace std;
+
+
+int main()
+{
+
+	//cout << fixed << setprecision(10);
+
+#ifndef ONLINE_JUDGE
+	freopen("i.txt", "r", stdin);
+	freopen("o.txt", "w", stdout);
+#endif
+
+	int t;
+
+	cin >> t;
+
+	while(t--)
+	{
+		int a,b,x;
+
+		cin >> a >> b >> x;
+
+		cout << (b-a)/x <<N;
+	}
+
+	
+	return 0;
+}
